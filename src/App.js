4688e3c4451b8ae1e395c566e3c99147a1219aa5
Patrick Gip <gippatrick@gmail.com>
2023-01-23 17:41:04 -0800

    Created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "../src/pages/ForgotPassword";
+import Home from "../src/pages/Home";
+import Offers from "../src/pages/Offers";
+import Profile from "../src/pages/Profile";
+import SignIn from "../src/pages/SignIn";
+import SignUp from "../src/pages/SignUp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
